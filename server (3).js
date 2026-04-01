b5c223de988c186a884c9da1fe1b852ccef5d5d3
srodrigue79 <24seventeenllc@gmail.com>
2026-03-31 22:06:59 -0500

    Update server (3).js

--- server (3).js
+++ server (3).js
@@ -4,8 +4,12 @@ const path = require('path');
 const PORT = process.env.PORT || 10000;
 const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end(html);
-}).listen(PORT, '0.0.0.0', () => {
-  console.log('24seventeen running on port ' + PORT);
-});
+  const cors = {'Access-Control-Allow-Origin':'*','Cache-Control':'no-cache'};
+  if (req.url === '/raw') {
+    res.writeHead(200, Object.assign({'Content-Type':'text/html; charset=utf-8'}, cors));
+    res.end(html);
+  } else {
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+    res.end(html);
+  }
+}).listen(PORT, '0.0.0.0', () => console.log('running on port ' + PORT));
