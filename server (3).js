065e7e9c34d2efe7551665c33d93f378a7e0e571
srodrigue79 <24seventeenllc@gmail.com>
2026-03-31 22:25:39 -0500

    Update server (3).js

--- server (3).js
+++ server (3).js
@@ -4,14 +4,13 @@ const path = require('path');
 const PORT = process.env.PORT || 10000;
 let html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
 
-// Runtime patch: restore pw-modal and debug-trigger if missing
 if (!html.includes('id="pw-modal"')) {
-  const pwModal = '<div id="pw-modal"><div id="pw-box"><h3>ENTER PASSWORD</h3><input id="pw-inp" type="password" placeholder="••••••••" onkeydown="if(event.key==='Enter')checkPw()"><div id="pw-err"></div><div class="pw-btns"><button class="pw-btn cancel" onclick="closePwModal()">Cancel</button><button class="pw-btn ok" onclick="checkPw()">Unlock</button></div></div></div>';
-  const debugBtn = '<button id="debug-trigger" onclick="exportDebug()">🔍 Debug</button>';
+  const pwModal = "<div id=\"pw-modal\"><div id=\"pw-box\"><h3>ENTER PASSWORD</h3><input id=\"pw-inp\" type=\"password\" placeholder=\"password\" onkeydown=\"if(event.keyCode===13)checkPw()\"><div id=\"pw-err\"></div><div class=\"pw-btns\"><button class=\"pw-btn cancel\" onclick=\"closePwModal()\">Cancel</button><button class=\"pw-btn ok\" onclick=\"checkPw()\">Unlock</button></div></div></div>";
+  const debugBtn = "<button id=\"debug-trigger\" onclick=\"exportDebug()\">Debug</button>";
   html = html.replace('onclick="openPwModal()"> Edit</button>', 'onclick="openPwModal()"> Edit</button>' + debugBtn + pwModal);
 }
 
 http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   res.end(html);
 }).listen(PORT, '0.0.0.0', () => console.log('24seventeen running on port ' + PORT));
